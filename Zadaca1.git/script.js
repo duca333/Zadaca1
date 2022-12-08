d9d8a434a0d99979b5d8d9bc6e2a4bbf1b8ddd58
duca333 <116892182+duca333@users.noreply.github.com>
2022-12-08 15:37:13 +0100

    Add files via upload

---
+++ Zadaca1.git/script.js
@@ -0,0 +1,70 @@
+'use strict'
+
+//zadatak 1
+
+console.log("Dusan Stojanovic");
+
+//zadatak 2
+
+const imeiPrezime = "Dusan Stojanovic";
+
+console.log(imeiPrezime);
+
+//zadatak 3 
+
+const br1 = 5;
+const br2 = 2;
+
+const zbir = br1 + br2;
+const razlika = br1 - br2;
+const prozivod = br1 * br2;
+const kolicnik = br1 / br2;
+const ostatak = br1 % br2;
+
+console.log(zbir,razlika,prozivod,kolicnik,ostatak);
+
+// zadatak 4 
+
+const broj1 = 5;
+const broj2 = 4;
+const broj3 = 3;
+const broj4 = 2;
+const broj5 = 1;
+
+const prvo = broj1 + broj2 + broj3 + broj4 + broj5;
+const drugo = broj1 - broj2 - broj3 - broj4 - broj5;
+const trece = broj1 * broj2 * broj3 * broj4 * broj5;
+
+console.log(prvo, drugo , trece);
+
+// zadatak 5
+
+const ime = "Dusan";
+const prezime = "Stojanovic";
+
+console.log (ime , " " , prezime);
+
+// zadatak 6 
+
+let danUSedmici = 6;
+
+if (danUSedmici == 1) {
+    console.log('Pon');
+} else if (danUSedmici == 2) {
+    console.log ('Uto');
+} else if (danUSedmici == 3) {
+    console.log ('Sri');
+} else if (danUSedmici == 4) {
+    console.log ('Cet');
+} else if (danUSedmici == 5) {
+    console.log ('Pet');
+} else if (danUSedmici == 6) {
+    console.log ('Sub');
+} else if (danUSedmici == 7) {
+    console.log ('Ned');
+} else {
+    console.log ('Error');
+}
+
+// zadatak 7
+
